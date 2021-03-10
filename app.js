95edd2169a361e5bb952394acf4798d17318f122
Lola Mukhamadova <00010442@wiut.uz>
2021-03-10 14:58:05 +0500

    init

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const app = express()
 
 app.set('view engine', 'pug')
 
+// dev process
+app.use('/static', express.static('public'))
+
 app.get('/', (req, res)=> {
 	res.render('home')
+})
+
+app.get('/create-note', (req, res)=> {
+	res.render('create')
 })
 
 app.listen(8000, err => {
